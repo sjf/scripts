0f3a408e7891307d6816bd9da204c71565a2e22e
sjf <sarah.fortune@gmail.com>
2019-06-20 20:37:39 -0700

    Add httpserver and chrome without CORS scripts

---
+++ httpserver.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+from http.server import BaseHTTPRequestHandler, HTTPServer
+#import urlparse
+
+# HTTPRequestHandler class
+class requestHandler(BaseHTTPRequestHandler):
+
+  # GET
+  def do_GET(self):
+    #parsed_path = urlparse.urlparse(self.path)
+
+    # Send response status code
+    self.send_response(200)
+
+    # Send headers
+    self.send_header('Content-type','text/json')
+    #self.send_header('')
+    self.end_headers()
+
+    # Send message back to client
+    message = '{"auto_refresh":true,"injector_proxy":true,"ipfs_cache":false,"origin_access":true,"proxy_access":false}'
+    # Write content as utf-8 data
+    self.wfile.write(bytes(message, "utf8"))
+    return
+
+def run():
+  print('starting server...')
+  server_address = ('127.0.0.1', 8081)
+  httpd = HTTPServer(server_address, requestHandler)
+  print('running server...')
+  httpd.serve_forever()
+
+run()
