9e3c9c2b8c7e5a83348657984804982c8a57ab08
Sarah Fortune <sarah.fortune@gmail.com>
2020-02-07 12:32:32 -0800

    .

---
+++ setproj.sh
@@ -0,0 +1,17 @@
+NAME=$1
+export CLOUDSDK_CORE_PROJECT=$NAME
+
+# function gc_properties() {
+#   CONFIG=$(gcloud config list)
+
+#   ENDPOINT=$(gc config get-value api_endpoint_overrides/cloudfunctions)
+#   PROJ=$(gc_project)
+#   RES="$PROJ"
+#   if [ -n "$ENDPOINT" ]; then
+#     RES="$RES(T)"
+#   fi
+#   echo "$RES"
+# }
+# gc_properties
+
+# \[\033[1;37\]
