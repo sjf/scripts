d239961740f72499aadf8dc2b130ad064bbf4886
Sarah Fortune <sarah.fortune@gmail.com>
2024-10-21 15:04:39 -0700

    sublime server

--- sublime_server.py
+++ sublime_server.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import sys
+import os
 import threading
 from http.server import BaseHTTPRequestHandler, HTTPServer
 from urllib.parse import urlparse, parse_qs
@@ -24,7 +25,7 @@ class RequestHandler(BaseHTTPRequestHandler):
         self.send(str("Filename rejected"), 500)
         return
 
-      local_file = get_local_path(remote_file)
+      local_file = self.get_local_path(remote_file)
 
       if local_file:
         open_sublime(local_file)
@@ -38,6 +39,14 @@ class RequestHandler(BaseHTTPRequestHandler):
       raise e
       return
 
+  def get_local_path(self, f):
+    mounts = get_mounts(self.server.remote_host)
+    for remote_prefix,local_prefix in mounts.items():
+      if f.startswith(remote_prefix):
+        return f.replace(remote_prefix, local_prefix)
+    return None
+    # raise Exception(f"Could not find mount point for {f}")
+
   def send(self, content, code=200):
     print(f"Response: {code} {content}")
     self.send_response(code)
@@ -45,60 +54,65 @@ class RequestHandler(BaseHTTPRequestHandler):
     self.end_headers()
     self.wfile.write(bytes(content+'\n', "utf8"))
 
-def get_mounts(output=False):
-  cmd = "mount|grep sjf|grep box: | cut -d' ' -f1,3|sed 's|box:||'"
+def get_mounts(remote_host, show=False):
+  cmd = f"mount|grep {remote_host}: | cut -d' ' -f1,3 | sed 's|{remote_host}:||'"
   output = subprocess.run(cmd, shell=True, check=True, stdout=subprocess.PIPE, text=True).stdout
   result = {}
   for l in output.split("\n"):
     if not l:
       continue
-    parts = l.split(" ")
-    result[parts[0]] = parts[1]
-  if output:
+    remote_dir,local_dir = l.split(" ")
+    result[remote_dir] = local_dir
+  if show:
     for k,v in result.items():
-      print(f"{k}:\t{v}")
+      print(f"{k}: {v}")
   return result
 
 def open_sublime(f):
   subprocess.run(f"/Users/sjf/scripts/sublime {f}", shell=True, check=True)
 
-def get_local_path(f):
-  mounts = get_mounts()
-  for remote_prefix,local_prefix in mounts.items():
-    if f.startswith(remote_prefix):
-      return f.replace(remote_prefix, local_prefix)
-  return None
-  # raise Exception(f"Could not find mount point for {f}")
-
 def start_tunnel(local_port, remote_host, remote_port):
   cmd = f"ssh -o ExitOnForwardFailure=yes  -N -R 127.0.0.1:{remote_port}:127.0.0.1:{local_port} {remote_host}"
   print(cmd)
-  subprocess.run(cmd, shell=True, check=True)
+  try:
+    subprocess.run(cmd, shell=True, check=True)
+  except subprocess.CalledProcessError as e:
+    print(e)
+    os._exit(0)
+
+def bind_from_string(s):
+  parts = s.split(":")
+  if len(parts) != 2:
+    raise Exception(f"Expected format host:port, got '{s}'")
+  return parts[0], parts[1]
 
 def run():
-  get_mounts(output=True)
-
-  bind = ('127.0.0.1', 8000)
+  local_host, local_port = '127.0.0.1', 8000
   remote_host, remote_port = 'box', 3456
   if len(sys.argv) > 1:
     try:
-      parts = sys.argv[1].split(':')
-      bind = (parts[0], int(parts[1]))
+      local_host, local_port = bind_from_string(sys.argv[1])
       if len(sys.argv) > 2:
-        parts = sys.argv[2].split(':')
-        remote_host, remote_port = (parts[0], int(parts[1]))
+        remote_host, remote_port = bind_from_string(sys.argv[2])
     except Exception as e:
       print("Usage: {sys.argv[0]} local_address:local_port [remote_host:remote_port]")
       raise e
 
+  get_mounts(remote_host, show=True)
+
+  bind = (local_host, local_port)
   print('starting server... http://%s:%d' % bind)
   httpd = HTTPServer(bind, RequestHandler)
+  httpd.remote_host = remote_host
   print('running server...')
   serving = threading.Thread(target=httpd.serve_forever)
+  # serving.daemon = True
   serving.start()
+
   if remote_host:
     local_port = bind[1]
     tunnel = threading.Thread(target=lambda:start_tunnel(local_port, remote_host, remote_port))
+    # tunnel.daemon = True
     tunnel.start()
 
 run()
