415954fa32cacefbc362a35f6bf5727c8c14474d
Sarah Fortune <sarah.fortune@gmail.com>
2025-02-10 20:03:32 -0800

    goaccess.sh

--- goaccess.sh
+++ goaccess.sh
@@ -7,7 +7,7 @@ if [[ ${1:-} == '-v' ]]; then
 fi
 
 LOG_FILES="$@"
-DEST=.
+DEST=${DEST:-.}
 CONFIG=$HOME/scripts/.goaccess.conf
 GEO_IP_DB=$HOME/scripts/.goaccess-geoip.mmdb
 SELF=$HOME/sjf/self.txt
@@ -32,31 +32,31 @@ mv ${TEMP_DIR}/access.log.1 ${TEMP_DIR}/access.log
 # and redirects output back to stderr. Because this tool sucks.
 $GOACCESS --persist -o ${DEST}/report_all.html ${TEMP_DIR}/access.log 2> >(grep -v 'Cleaning up resources...' >&2)
 
-$GOACCESS --restore -o ${DEST}/report.html \
-  --ignore-panel=NOT_FOUND \
-  --ignore-panel=REQUESTS_STATIC \
-  --ignore-panel=VISIT_TIMES \
-  --ignore-panel=OS \
-  --ignore-panel=REQUESTS \
-  --ignore-panel=HOSTS \
-  --ignore-panel=KEYPHRASES \
-  --ignore-panel=ASN \
-  --ignore-panel=REFERRERS \
-  --ignore-panel=REFERRING_SITES \
-  --ignore-panel=BROWSERS \
-  2> >(grep -v 'Cleaning up resources...' >&2)
-$GOACCESS --restore -o ${DEST}/report_clients.html \
-  --ignore-panel=REQUESTS_STATIC \
-  --ignore-panel=NOT_FOUND \
-  --ignore-panel=VISIT_TIMES \
-  --ignore-panel=VIRTUAL_HOSTS \
-  --ignore-panel=STATUS_CODES \
-  --ignore-panel=REMOTE_USER \
-  --ignore-panel=CACHE_STATUS \
-  --ignore-panel=MIME_TYPE \
-  --ignore-panel=ASN \
-  --ignore-panel=TLS_TYPE \
-  2> >(grep -v 'Cleaning up resources...' >&2)
+# $GOACCESS --restore -o ${DEST}/report.html \
+#   --ignore-panel=NOT_FOUND \
+#   --ignore-panel=REQUESTS_STATIC \
+#   --ignore-panel=VISIT_TIMES \
+#   --ignore-panel=OS \
+#   --ignore-panel=REQUESTS \
+#   --ignore-panel=HOSTS \
+#   --ignore-panel=KEYPHRASES \
+#   --ignore-panel=ASN \
+#   --ignore-panel=REFERRERS \
+#   --ignore-panel=REFERRING_SITES \
+#   --ignore-panel=BROWSERS \
+#   2> >(grep -v 'Cleaning up resources...' >&2)
+# $GOACCESS --restore -o ${DEST}/report_clients.html \
+#   --ignore-panel=REQUESTS_STATIC \
+#   --ignore-panel=NOT_FOUND \
+#   --ignore-panel=VISIT_TIMES \
+#   --ignore-panel=VIRTUAL_HOSTS \
+#   --ignore-panel=STATUS_CODES \
+#   --ignore-panel=REMOTE_USER \
+#   --ignore-panel=CACHE_STATUS \
+#   --ignore-panel=MIME_TYPE \
+#   --ignore-panel=ASN \
+#   --ignore-panel=TLS_TYPE \
+#   2> >(grep -v 'Cleaning up resources...' >&2)
 
 rm -rf $TEMP_DIR
   # --ignore-panel=VISITORS \
