d4e6e31589aa2b021d69486aa0ac2575b2aabf68
Sarah Fortune <sarah.fortune@gmail.com>
2025-03-04 17:30:23 -0800

    goaccess

--- goaccess.sh
+++ goaccess.sh
@@ -15,7 +15,7 @@ SELF=$HOME/sjf/self.txt
 TEMP_DIR=$(mktemp -d /tmp/goaccess.XXXX)
 mkdir -p $TEMP_DIR
 
-GOACCESS="goaccess --no-progress -p $CONFIG --geoip-database=$GEO_IP_DB --db-path=${TEMP_DIR}"
+GOACCESS="goaccess --no-progress -p $CONFIG --geoip-database=$GEO_IP_DB --db-path=${TEMP_DIR} --ignore-crawlers"
 
 # Remove internal requests
 if [ -f $SELF ]; then
@@ -31,6 +31,18 @@ mv ${TEMP_DIR}/access.log.1 ${TEMP_DIR}/access.log
 # remove the non-error message in a subshell (to preserve the exit code)
 # and redirects output back to stderr. Because this tool sucks.
 $GOACCESS --persist -o ${DEST}/report_all.html ${TEMP_DIR}/access.log 2> >(grep -v 'Cleaning up resources...' >&2)
+
+grep '" 404 ' ${TEMP_DIR}/access.log > ${TEMP_DIR}/access.404.log
+$GOACCESS -o ${DEST}/report_404.html ${TEMP_DIR}/access.404.log 2> >(grep -v 'Cleaning up resources...' >&2)
+
+grep '" 5[0-9][0-9] ' ${TEMP_DIR}/access.log > ${TEMP_DIR}/access.5xx.log
+$GOACCESS -o ${DEST}/report_5xx.html ${TEMP_DIR}/access.5xx.log 2> >(grep -v 'Cleaning up resources...' >&2)
+
+grep '" 200 ' ${TEMP_DIR}/access.log > ${TEMP_DIR}/access.200.log
+$GOACCESS -o ${DEST}/report_200.html ${TEMP_DIR}/access.200.log 2> >(grep -v 'Cleaning up resources...' >&2)
+
+cat $1 | egrep -v $(cat $SELF) > ${TEMP_DIR}/access.recent.log
+$GOACCESS -o ${DEST}/report_recent.html ${TEMP_DIR}/access.recent.log 2> >(grep -v 'Cleaning up resources...' >&2)
 
 # $GOACCESS --restore -o ${DEST}/report.html \
 #   --ignore-panel=NOT_FOUND \
