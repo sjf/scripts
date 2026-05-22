694e49cbde0f9830f09756e914c9d5e9f30a2ce2
Sarah Fortune <sarah.fortune@gmail.com>
2026-05-22 15:29:01 -0700

    rm_mac_crap.sh

--- rm_mac_crap.sh
+++ rm_mac_crap.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 set -eux
 TIME=${1:-0}
-for dir in ~/mb ~/sbb ~/sjf ~/scripts;do
+#for dir in ~/mb ~/sbb ~/sjf ~/scripts;do
+for dir in $(pwd); do
   find $dir -name ._\*  -type f -mmin +${TIME} -print0 | xargs -0 -r rm -v
 done
